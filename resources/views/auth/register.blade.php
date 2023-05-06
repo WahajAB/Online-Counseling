93777cb4543c00f9e4b245962a51e429f76ae9cb
Wahaj Ahmed Butt <wahajahmedbutt@ymail.com>
2023-05-06 23:04:22 +0500

    Latest

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,7 +10,7 @@
     <div class="row justify-content-center">
         <div class="col-md-9">
             <div class="card shadow">
-                <div class="card-header text-center" style="font: 12pt bold; font-size: 2rem;">{{ __('Register') }}</div>
+                <div class="card-header text-center" style="font: 12pt bold; font-size: 1.5rem;">{{ __('Register') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
