11df30febfefe6077b67511fd646db39980ce74e
Wahaj Ahmed Butt <wahajahmedbutt@ymail.com>
2023-05-09 08:24:53 +0500

    Latest

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
+<link href="{{ asset('css/green-theme.css') }}" rel="stylesheet">
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div style="background-color:hsl(183, 100%, 36%); text-align:center; color:white;" class="card-header">{{ __('Reset Password') }}</div>
+                <div style="font-weight: 700; font-size: 1.6rem;" class="card-header text-center">{{ __('Reset Password') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -18,7 +19,7 @@
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address:') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -34,7 +35,7 @@
                         <div class="row mb-0">
                             <div style="color:hsl(183, 100%, 36%);" class="col-md-6 offset-md-4">
                                 <button style="background-color:hsl(183, 100%, 36%);" type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
+                                    {{ __('Send Password Reset Request') }}
                                 </button>
                             </div>
                         </div>
