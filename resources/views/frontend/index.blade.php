8c1c5255b953ca405394a60efdc4b6ff730a50ab
Wahaj Ahmed Butt <wahajahmedbutt@ymail.com>
2023-05-11 00:04:49 +0500

    HEHE

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -2,7 +2,5 @@
 @section('content')
 <link href="{{ asset('css/green-theme.css') }}" rel="stylesheet">
 
-<div class="text" style="text-align: center">insert front page here later</div>
-
 
 @endsection
