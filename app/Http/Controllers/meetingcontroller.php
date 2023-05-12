c53d1cbe8bd475d45410448a697c50f1ad4f7bd2
BasharSiddiqui <105674805+BasharSiddiqui@users.noreply.github.com>
2023-05-13 01:00:42 +0500

    LATEST URLS ADDED

--- app/Http/Controllers/meetingcontroller.php
+++ app/Http/Controllers/meetingcontroller.php
@@ -24,7 +24,7 @@ class MeetingController extends Controller
         $message->counselor_id = $id;
         $name = auth()->user()->name;
         $message->subject = "$name is inviting you to a meeting!";
-        $message->message = "Please Copy/Paste the Following Link In Your Browser to Join the Meeting: http://127.0.0.1:8000/meeting/{$roomName}";
+        $message->message = "Click this URL to Join the Meeting: http://127.0.0.1:8000/meeting/{$roomName}";
 
         $message->save();
         
