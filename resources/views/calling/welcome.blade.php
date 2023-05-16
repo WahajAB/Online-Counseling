0039ae2782fd6a6dc33f418716ad26dce734b9d8
BasharSiddiqui <105674805+BasharSiddiqui@users.noreply.github.com>
2023-05-16 16:51:34 +0500

    ad

--- resources/views/calling/welcome.blade.php
+++ resources/views/calling/welcome.blade.php
@@ -1,52 +1,56 @@
 @extends('layouts.app')
 @section('content')
-    @vite('resources/css/app.css')
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
-    <link href="{{ asset('css/green-theme.css') }}" rel="stylesheet">
+<link href="{{ asset('css/green-theme.css') }}" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="card col-md-8 p-0">
-                <div class="card-header text-center" style="font-weight:700; font-size: 1.5rem;">{{ __('Meeting Room') }}</div>
-                @if (session('failure'))
+<style>
+    .button-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 10px;
+        margin-top: 20px;
+    }
+</style>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="card col-md-8 p-0">
+            <div class="card-header text-center" style="font-weight:700; font-size: 1.5rem;">{{ __('Meeting Room') }}</div>
+            @if (session('failure'))
                 <div class="alert alert-danger text-center" style="margin-bottom: -1rem;" role="alert">
                     {{ session('failure') }}
                 </div>
-                @endif
-                <div class="card-body" style="display: flex;  justify-content: center; align-items: center;">
-
-                            <form method="post" action="{{ route('validateMeeting', ['id'=>$counselor]) }}">
-                                {{ csrf_field() }}
-                                <div class="mt-10 mb-11 flex">
-                                    <div class="rounded-md shadow-sm relative flex items-stretch flex-grow focus-within:z-10">
-                                        <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                                            <!-- Heroicon name: solid/users -->
-                                            <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg"
-                                                viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                                <path
-                                                    d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
-                                            </svg>
-                                        </div>
-                                        <input type="text" name="meetingId" id="meetingId"
-                                            class="focus:ring-indigo-500 focus:border-indigo-500 block w-full rounded-none rounded-l-md pl-10 sm:text-sm border-gray-300"
-                                            style = "padding-right: 5rem;" placeholder="Meeting ID">
-                                    </div>
-                                    <button type="submit" class="btn btn-primary ml-2">
-                                        <span>Join Meeting</span>
-                                    </button>
-                                </div>
-                            </form>
-
-
-                        <div class="mt-10 mb-11 pl-1">
-                            <form method="post" action="{{ route('createMeeting', ['id'=>$counselor]) }}">
-                                {{ csrf_field() }}
-                                <button type="submit" class="btn btn-primary">Create New Meeting</button>
-                            </form>
+            @endif
+            <div class="card-body justify-content-center align-items-center">
+                <form method="post" action="{{ route('validateMeeting') }}">
+                    {{ csrf_field() }}
+                    <div class="mt-3 mb-3">
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">
+                                    <svg class="bi bi-people-fill" xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" viewBox="0 0 16 16">
+                                        <path d="M5 3a3 3 0 1 1 0 6 3 3 0 0 1 0-6zm7 0a3 3 0 1 1 0 6 3 3 0 0 1 0-6zm-7 8s-4 0-4 4h10c0-4-4-4-4-4zm7 0c-2.16 0-4 1.239-4 3h8c0-1.761-1.84-3-4-3z"/>
+                                    </svg>
+                                </span>
+                            </div>
+                            <input type="text" name="meetingId" id="meetingId" class="form-control"
+                                   style="padding-right: 5rem;" placeholder="Meeting ID">
                         </div>
                     </div>
-                </div>
+                    <div style="display: flex;justify-content: center;align-items: center;gap: 10px;margin-top: 20px;" class="button-container">
+                        <button type="submit" class="btn btn-primary">
+                            <span>Join Meeting</span>
+                        </button>
+                        <form method="post" action="{{ route('createMeeting', ['id'=>$counselor]) }}">
+                            {{ csrf_field() }}
+                            <button type="submit" class="btn btn-primary ">Create New Meeting</button>
+                        </form>
+                    </div>
+                </form>
             </div>
         </div>
+    </div>
+</div>
 @endsection
