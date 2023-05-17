626852f3f3ab1befffb202c825d1d6221fdaa430
BasharSiddiqui <105674805+BasharSiddiqui@users.noreply.github.com>
2023-05-17 20:35:22 +0500

    commit

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -10,8 +10,8 @@
                 <p class="fs-5" style="margin-top: -2rem; font-weight: 400;">Welcome To Palingenesis!</p>
                 <h1 style="font: 4rem bold; font-weight: 300; font-family: Nunito;">Convenient and affordable therapy, anywhere and anytime.</h1>
             </div>
-            <div class="col-md-6">
-                <img src="{{ url('frontend/images/hero-banner.png') }}" loading="eager" class="w-90" style="overflow: hidden;">
+            <div style="padding-left: 7.5rem;" class="col-md-6">
+                <img src="{{ url('frontend/images/hero-banner13.png') }}" loading="eager" class="w-90" height="600px;" width="600px" style="overflow: hidden;height:600px;width:600px;">
             </div>
         </div>
     </div>
@@ -21,7 +21,7 @@
         <div class="col">
             <div class="card h-100">
                 <div class="card-body">
-                    <img src="{{ url('frontend/images/icon-1.png') }}" width="71" height="71" loading="lazy" alt="icon">
+                    <img src="{{ url('frontend/images/icon-20.png') }}" width="71" height="71" loading="lazy" alt="icon">
                     <h3 style="text-align: justify;" class="card-title">Confidential</h3>
                     <p style="text-align: justify; font-family:'Nunito';" class="card-text">Rest assured knowing that your sessions are completely confidential, ensuring a safe and secure therapeutic space.</p>
                 </div>
@@ -30,7 +30,7 @@
         <div class="col">
             <div class="card h-100">
                 <div class="card-body">
-                    <img src="{{ url('frontend/images/icon-2.png') }}" width="71" height="71" loading="lazy" alt="icon">
+                    <img src="{{ url('frontend/images/accessible.png') }}" width="71" height="71" loading="lazy" alt="icon">
                     <h3 class="card-title">Accessible</h3>
                     <p style="text-align: justify;" class="card-text">Enjoy therapy that is readily available and easily accessible to help you prioritize your mental well-being.</p>
                 </div>
@@ -39,7 +39,7 @@
         <div class="col">
             <div class="card h-100">
                 <div class="card-body">
-                    <img src="{{ url('frontend/images/icon-3.png') }}" width="71" height="71" loading="lazy" alt="icon">
+                    <img src="{{ url('frontend/images/empathy.png') }}" width="71" height="71" loading="lazy" alt="icon">
                     <h3 class="card-title">Empathy</h3>
                     <p style="text-align: justify;" class="card-text">Experience the compassionate support of dedicated professionals who empathize and understand your unique journey.</p>
                 </div>
@@ -48,7 +48,7 @@
         <div class="col">
             <div class="card h-100">
                 <div class="card-body">
-                    <img src="{{ url('frontend/images/icon-4.png') }}" width="71" height="71" loading="lazy" alt="icon">
+                    <img src="{{ url('frontend/images/virtual.png') }}" width="71" height="71" loading="lazy" alt="icon">
                     <h3 class="card-title">Virtual</h3>
                     <p style="text-align: justify;" class="card-text">Experience therapy from the comfort of your own space, eliminating the need for travel or commuting.</p>
                 </div>
@@ -58,25 +58,24 @@
 </section>
 
 <section class="container py-5">
-    <div class="text-center pb-5">
+    <div class="text-center pb-3">
         <p class="fs-2">World Class Experts Offering:</p>
     </div>
     <div class="row text-center">
         <div class="col">
-
             <div class="card-body">
-                <img src="{{ url('frontend/images/icon-5.png') }}" width="71" height="71" loading="lazy" alt="icon">
-                <h3 class="card-title">Mindfulness-Based Therapy</h3>
-                <p class="card-text">Incorporate mindfulness practices to enhance self-awareness and reduce stress and anxiety.</p>
+                <img src="{{ url('frontend/images/MBT.png') }}" width="400" height="400" loading="lazy" alt="icon">
+                <h2 class="card-title pb-3">Mindfulness-Based Therapy</h2>
+                <p class="card-text h5">Incorporate mindfulness practices to enhance self-awareness and reduce stress and anxiety.</p>
             </div>
 
         </div>
         <div class="col">
 
             <div class="card-body">
-                <img src="{{ url('frontend/images/icon-6.png') }}" width="71" height="71" loading="lazy" alt="icon">
-                <h3 class="card-title">Solution-Focused Brief Therapy (SFBT)</h3>
-                <p class="card-text">Focus on identifying solutions and setting achievable goals in a time-limited manner.</p>
+                <img src="{{ url('frontend/images/SFBT.png') }}" width="400" height="400" loading="lazy" alt="icon">
+                <h2 class="card-title pb-3">Solution-Focused Brief Therapy (SFBT)</h2>
+                <p class="card-text h5">Focus on identifying solutions and setting achievable goals in a time-limited manner.</p>
 
             </div>
         </div>
